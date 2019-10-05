92ecb2c50b6a4e87cadd064023ccb5bf5b43181a
nicdom23 <56066177+nicdom23@users.noreply.github.com>
2019-10-05 17:22:38 +0200

    Update remote_file.cc

--- remote_file.cc
+++ remote_file.cc
@@ -2,7 +2,7 @@
 
 int main(){
 std::cout<<"This code was written online";
-std::cout<<"This code was added in the local repository"
+std::cout<<"This code was added in the local repository";
 
 return 0;
 }
